69714efd1fd45f986d17b97e348d36799025d0d1
Steven Tidd <stidd@aacps.org>
2020-10-06 10:50:45 +0000

    Initial commit

---
+++ index.java
@@ -0,0 +1,25 @@
+void setup() {
+	size(500, 500);
+}
+
+void draw() {
+// Step 1: Write code that draws the following screenshots 
+// with hard-coded values. (Feel free to use colors 
+// instead of grayscale.)
+
+// Step 2: Replace all of the hard-coded numbers with 
+// variables.
+
+// Step 3: Write assignment operations in draw() that 
+// change the value of the variables.
+// For example, “variable1 = variable1 + 2;”. 
+// Try different expressions and see what happens!
+
+  background(255);
+  stroke(0);
+  fill(175);
+  ellipse(50,50,50,50);
+  ellipse(150,50,50,50);
+  ellipse(50,150,50,50);
+  ellipse(150,150,50,50);
+}
